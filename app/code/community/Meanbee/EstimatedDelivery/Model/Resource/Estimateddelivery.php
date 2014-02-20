b3cfee324aab4380538ed6ad287f0b967a8a927a
Martin Shaw <martin.shaw@meanbee.com>
2014-02-20 14:00:56 +0000

    Move shipping methods in another table and implement SQL to load into an array

--- app/code/community/Meanbee/EstimatedDelivery/Model/Resource/Estimateddelivery.php
+++ app/code/community/Meanbee/EstimatedDelivery/Model/Resource/Estimateddelivery.php
@@ -9,4 +9,34 @@ class Meanbee_EstimatedDelivery_Model_Resource_Estimateddelivery extends Mage_Co
     protected function _construct() {
         $this->_init('meanbee_estimateddelivery/estimateddelivery', 'entity_id');
     }
+
+    public function load(Mage_Core_Model_Abstract $object, $value, $field = null) {
+        if (is_null($field)) {
+            $field = $this->getIdFieldName();
+        }
+
+        $read = $this->_getReadAdapter();
+        if ($read && !is_null($value)) {
+            $select = $this->_getLoadSelect($field, $value, $object);
+
+            // Add our shipping methods from another table into the load select. Retrieve them as a comma separated list.
+            $groupConcat = new Zend_Db_Expr("group_concat(shipping_method separator ',')");
+            $select->columns(array('shipping_methods' => $groupConcat));
+            $select->join('meanbee_estimateddelivery_method', 'entity_id = estimated_delivery_id', array());
+            $select->group('entity_id');
+
+            $data = $read->fetchRow($select);
+
+            if ($data) {
+                // Convert the comma separated list into an array
+                $data['shipping_methods'] = explode(',', $data['shipping_methods']);
+                $object->setData($data);
+            }
+        }
+
+        $this->unserializeFields($object);
+        $this->_afterLoad($object);
+
+        return $this;
+    }
 }
--- app/code/community/Meanbee/EstimatedDelivery/sql/meanbee_estimateddelivery_setup/mysql4-install-1.0.0.php
+++ app/code/community/Meanbee/EstimatedDelivery/sql/meanbee_estimateddelivery_setup/mysql4-install-1.0.0.php
@@ -5,10 +5,11 @@ $installer = $this;
 
 $installer->startSetup();
 
-$table = $installer->getTable('meanbee_estimateddelivery/estimateddelivery');
+$mainTable = $installer->getTable('meanbee_estimateddelivery/estimateddelivery');
+$methodTable = $installer->getTable('meanbee_estimateddelivery/estimateddelivery_method');
 
 $sql = <<<SQL
-CREATE TABLE `$table` (
+CREATE TABLE `$mainTable` (
     `entity_id` int(11) unsigned NOT NULL auto_increment,
     `dispatch_preparation` int(11) DEFAULT NULL,
     `dispatchable_days` varchar(256) DEFAULT NULL,
@@ -16,8 +17,16 @@ CREATE TABLE `$table` (
     `estimated_delivery_from` int(11) DEFAULT NULL,
     `estimated_delivery_to` int(11) DEFAULT NULL,
     `deliverable_days` varchar(256) DEFAULT NULL,
-    `shipping_method` varchar(256) DEFAULT NULL,
     PRIMARY KEY (`entity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `$methodTable` (
+    `shipping_method` varchar(256) NOT NULL,
+    `estimated_delivery_id` int(11) unsigned NOT NULL,
+    PRIMARY KEY (`shipping_method`,`estimated_delivery_id`),
+    CONSTRAINT `FK_estimated_delivery_id` FOREIGN KEY (`estimated_delivery_id`) REFERENCES `$mainTable` (`entity_id`) ON DELETE CASCADE
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 SQL;
 
