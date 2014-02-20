75835b6aa8c581cbcc20f3846ac45785d22978f3
Martin Shaw <martin.shaw@meanbee.com>
2014-02-20 15:45:00 +0000

    Add the shipping method data in a collection call

--- app/code/community/Meanbee/EstimatedDelivery/Model/Resource/Estimateddelivery/Collection.php
+++ app/code/community/Meanbee/EstimatedDelivery/Model/Resource/Estimateddelivery/Collection.php
@@ -5,16 +5,40 @@ class Meanbee_EstimatedDelivery_Model_Resource_Estimateddelivery_Collection exte
 
     protected function _construct() {
         $this->_init('meanbee_estimateddelivery/estimateddelivery');
+        $this->_methodTable = $this->getTable('meanbee_estimateddelivery/estimateddelivery_method');
     }
+
+    protected function _initSelect() {
+        parent::_initSelect();
+
+        $groupConcat = new Zend_Db_Expr("group_concat(shipping_method separator ',')");
+        $this->_select->columns(array('shipping_methods' => $groupConcat));
+        $this->_select->joinLeft("$this->_methodTable", 'entity_id = estimated_delivery_id', array());
+        $this->_select->group('entity_id');
+
+        return $this;
+    }
+
 
     protected function _afterLoad() {
         parent::_afterLoad();
         foreach ($this->_items as $item) {
+            // Unserialize the relevant fields
             foreach ($this->_serializedFields as $field) {
                 $unserializedData = $item->getData($field);
                 $item->setData($field, unserialize($unserializedData));
             }
+
+            if ($methods = $item->getShippingMethods()) {
+                // Extract the shipping method data into an array
+                $methods = explode(',', $methods);
+            } else {
+                $methods = array();
+            }
+
+            $item->setData('shipping_methods', $methods);
         }
+
         return $this;
     }
 }
